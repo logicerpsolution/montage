5bc90c204f0d4bccbcc643ff7e5b5da700c5971e
unknown <logicerpsolutions1990@gmail.com>
2015-12-30 10:42:56 +0530

    First commit

---
+++ ui/signin.reel/signin.js
@@ -0,0 +1,11 @@
+/**
+ * @module ui/signin.reel
+ */
+ var Montage = require("montage/core/core").Montage,
+ Component = require("montage/ui/component").Component;
+
+/**
+ * @class Signin
+ * @extends Component
+ */
+exports.Signin = Montage.create(Component);
